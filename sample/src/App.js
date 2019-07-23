185e0988b255608b39657010f08332e93375fa36
Rajan Lamichhane <rajanlami@gmail.com>
2019-07-23 20:18:21 +0100

    girish format

--- sample/src/App.js
+++ sample/src/App.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import './App.css';
-import sampledata from './sampledata.json'
-import SampleTable from './SampleTable'
-
+import React from "react";
+import "./App.css";
+import sampledata from "./sampledata.json";
+import SampleTable from "./SampleTable";
 
 function App() {
   return (
--- sample/src/SampleTable.js
+++ sample/src/SampleTable.js
@@ -1,15 +1,53 @@
-import React from 'react'
-import SampleTableItem from './SampleTableItem'
+import React from "react";
+import SampleTableItem from "./SampleTableItem";
 
 const SampleTable = ({ sampleData }) => {
-    let allEnvs = sampleData.find(item => item.allEnvs.length).allEnvs || []
-    return <>
-        {
-            allEnvs.length && allEnvs.map((item, idx) => {
-                return <SampleTableItem key={idx} env={item} sampleData={sampleData} />
-            })
-        }
-    </>
+  const allEnvs = sampleData.find(item => item.allEnvs.length).allEnvs || [];
+  const envData =
+    sampleData.find(item => allEnvs.includes(item.env)).data || [];
+
+  const productNames =
+    envData.length &&
+    envData.map(item => {
+      return item.name;
+    });
+
+  const getHeader = () => {};
+  return (
+    <table>
+      <tr>
+        <td> </td>
+        {allEnvs.length &&
+          allEnvs.map((item, idx) => {
+            return (
+              <th key={idx} scope="col">
+                {item}
+              </th>
+            );
+          })}
+      </tr>
+      {productNames.length &&
+        productNames.map((productName, idx) => {
+          return (
+            <tr key={idx}>
+              <th scope="row">{productName}</th>
+              {allEnvs.length &&
+                allEnvs.map((item, idx) => {
+                  let envProductsData =
+                    sampleData.find(product => product.env === item).data || [];
+                  let productVersion =
+                    envProductsData.length &&
+                    envProductsData.find(
+                      productData => productData.name === productName
+                    ).version;
+
+                  return <td key={idx}>{productVersion}</td>;
+                })}
+            </tr>
+          );
+        })}
+    </table>
+  );
 };
 
-export default SampleTable
+export default SampleTable;
